b01c1e70064f8a8b2e4197eb6974832c7cb9cdeb
Leoudah <110877097+Leoudah@users.noreply.github.com>
2025-12-18 12:49:22 +0800

    co

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+
+    return (
+        <>
+            <p>Email</p>
+            <input type="text" />
+            <p>Password</p>
+            <input type="password" name="" id="" />
+        </>
+    )
+}
+
+export default Login
