e9de487dc0b956130dbb3a50307aaad8392118f1
Hesa Dharma <hesamade@gmail.com>
2026-01-02 18:02:35 +0800

    dashboard dosen

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,10 @@
-import { useLogout } from "../hooks/userLogout";
-
 function Navbar() {
-  const logout = useLogout();
 
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">SINTA</h1>
+      <h1 className="text-xl font-bold">DASHBOARD DOSEN SINTA</h1>
       <div className="flex space-x-4">
       </div>
-      <button
-        onClick={logout}
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Logout
-      </button>
     </nav>
   );
 }
--- frontend/src/components/SidebarDosen.jsx
+++ frontend/src/components/SidebarDosen.jsx
@@ -1,36 +1,26 @@
 function SidebarDosen() {
   return (
     <div className="w-64 bg-gray-800 text-white h-screen p-4">
-      <h2 className="text-xl font-bold mb-4">Menu</h2>
+      <h2 className="text-xl font-bold mb-4">Quick Menu</h2>
       <ul className="space-y-2">
-        <li>
-          <a href="#" className="block py-2 px-4 rounded hover:bg-gray-700">
-            Dashboard
+        <li className="rounded bg-blue-600">
+          <a href="#" className="block py-2 px-4 rounded hover:bg-blue-700 flex items-center">
+            <span className="mr-2 text-xl">+</span> Tambah Publikasi
           </a>
         </li>
-        <li>
+        <li className="border border-white/20 rounded">
           <a href="#" className="block py-2 px-4 rounded hover:bg-gray-700">
-            Mahasiswa
+            Beranda
           </a>
         </li>
-        <li>
-          <a href="#" className="block py-2 px-4 rounded hover:bg-gray-700">
-            Mata Kuliah
-          </a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-4 rounded hover:bg-gray-700">
-            Nilai
-          </a>
-        </li>
-        <li>
+        <li className="border border-white/20 rounded">
           <a href="#" className="block py-2 px-4 rounded hover:bg-gray-700">
             Profil
           </a>
         </li>
-        <li>
+        <li className="border border-white/20 rounded">
           <a href="#" className="block py-2 px-4 rounded hover:bg-gray-700">
-            Logout
+            Kelola Publikasi
           </a>
         </li>
       </ul>
