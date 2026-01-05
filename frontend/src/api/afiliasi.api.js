9dc401b6ef1139d74424698db524050610ed6779
cindysri <cindyodenni@gmail.com>
2026-01-05 17:09:36 +0700

    Update afiliasi.api.js

--- frontend/src/api/afiliasi.api.js
+++ frontend/src/api/afiliasi.api.js
@@ -1,1 +1,10 @@
+export const getAllAfiliasi = async () => {
+  const res = await fetch("/api/afiliasi");
+  return res.json();
+};
+
+export const searchAfiliasi = async (keyword) => {
+  const res = await fetch(`/api/afiliasi/search?keyword=${keyword}`);
+  return res.json();
+};
 
