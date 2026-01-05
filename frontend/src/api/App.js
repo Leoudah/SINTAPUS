ee82d75be01dd9b5aa8359705b9ed785aceaa647
cindysri <cindyodenni@gmail.com>
2026-01-05 17:14:15 +0700

    Update App.js

--- frontend/src/api/App.js
+++ frontend/src/api/App.js
@@ -1,1 +1,14 @@
+import AdminPage from "./pages/AdminPage";
+import DosenPage from "./pages/DosenPage";
+import AfiliasiPage from "./pages/AfiliasiPage";
+
+function App() {
+  return (
+    <>
+      <AdminPage />
+    </>
+  );
+}
+
+export default App;
 
